90345592d0cd4dc616d619d81ea9eb562f5c0eb8
sviatoslavtataryn <www.t.svyatoslav55@gmail.com>
2024-06-26 14:57:25 +0200

    create implementation fun removeChars

--- src/main/kotlin/mate/academy/RemoveChars.kt
+++ src/main/kotlin/mate/academy/RemoveChars.kt
@@ -1,6 +1,8 @@
 package mate.academy
 
+fun main() {
+    println(removeChars("string"))
+}
 fun removeChars(str: String): String {
-    // Implement this function
-    return ""
+    return str.drop(1).dropLast(1)
 }
